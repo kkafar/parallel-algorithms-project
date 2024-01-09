34cbe9777b67d0494f1e7b41062763243cb94588
Kacper Kafara <kacperkafara@gmail.com>
2024-01-09 13:57:46 +0100

    Improve data collection

--- langevin_equation.cu
+++ langevin_equation.cu
@@ -5,13 +5,15 @@
 #include <numeric>
 #include <ranges>
 #include <cmath>
+#include <memory>
+#include <random>
+#include <tuple>
 
-__global__ void langevin_equation(float *output, int n, float gamma, unsigned long long seed) {
+__global__ void langevin_equation(float *output, float dt, float gamma, unsigned long long seed) {
     int idx = blockIdx.x;
     float x = 0.0;
     float v = 0.0;
     float t = 0.0;
-    float dt = 0.001;
 
     curandState state;
     curand_init(seed, idx, 0, &state);
@@ -29,44 +31,56 @@ __global__ void langevin_equation(float *output, int n, float gamma, unsigned lo
     output[idx] = t;
 }
 
-int main() {
-    float gamma = 0.33;
+struct sim_result {
+    float dt;
+    float avg_time;
+    float std_dev;
+};
 
-    // Set the size of the array
-    int n = 24;
+
+int main() {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<unsigned long long> dist(1, 10000); // for seed generation...
+
+    const float gamma = 0.33;
+    int par_paths = 24;
+    std::vector<float> dt_list{0.001, 0.002, 0.003};
 
     // Allocate memory on the host
-    float *h_output = (float*)malloc(n * sizeof(float));
+    const auto h_output = std::make_shared<float[]>(par_paths);
 
     // Allocate memory on the device
     float *d_output;
-    cudaMalloc((void**)&d_output, n * sizeof(float));
+    cudaMalloc((void**)&d_output, par_paths * sizeof(float));
 
-    // Set mean, standard deviation, and seed
-    unsigned long long seed = 1234;
-
-    // Launch the CUDA kernel
     int blockSize = 1;
-    int numBlocks = (n + blockSize - 1) / blockSize;
+    int numBlocks = (par_paths + blockSize - 1) / blockSize;
 
-    langevin_equation<<<numBlocks, blockSize>>>(d_output, n, gamma, seed);
+    std::vector<sim_result> results{};
 
-    // Copy the results back to the host
-    cudaMemcpy(h_output, d_output, n * sizeof(float), cudaMemcpyDeviceToHost);
+    for (float dt : dt_list) {
+        unsigned long long seed = dist(mt);
+        langevin_equation<<<numBlocks, blockSize>>>(d_output, dt, gamma, seed);
 
-    const float total_avg = std::accumulate(h_output, h_output + n, 0.0f) / static_cast<float>(n);
-    const auto square_fn = [total_avg](auto val) { return (val - total_avg) * (val - total_avg); };
-    const float stddev = std::sqrt(std::transform_reduce(h_output, h_output + n, 0.0f, std::plus{}, square_fn) / (n - 1));
+        // Copy the results back to the host
+        cudaMemcpy(h_output.get(), d_output, par_paths * sizeof(float), cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < n; ++i) {
-        printf("%f ", h_output[i]);
+        const float total_avg = std::accumulate(h_output.get(), h_output.get() + par_paths, 0.0f) / static_cast<float>(par_paths);
+        const auto square_fn = [total_avg](auto val) { return (val - total_avg) * (val - total_avg); };
+        const float stddev = std::sqrt(std::transform_reduce(h_output.get(), h_output.get() + par_paths, 0.0f, std::plus{}, square_fn) / (par_paths - 1));
+        results.push_back({dt, total_avg, stddev});
+    }
+
+    std::printf("dt,avg,std")
+    for (const auto& result : results) {
+        std::printf("%.2f,%.2f,%.2f\n", result.dt, result.avg_time, result.std_dev);
     }
     printf("\n");
-    printf("Avg: %.2f with std: %.2f\n", total_avg, stddev);
+
 
     // Free device and host memory
     cudaFree(d_output);
-    free(h_output);
 
     return 0;
 }
