1633c46eb2ea45d5127a809b813eb2fe9abea423
Kacper Kafara <kacperkafara@gmail.com>
2024-01-11 18:41:15 +0100

    Measure nanoseconds

--- langevin_equation.cu
+++ langevin_equation.cu
@@ -89,7 +89,7 @@ int main() {
             auto start = std::chrono::high_resolution_clock::now();
             langevin_equation<<<numBlocks, 1>>>(d_output, dt, gamma, seed);
             auto stop = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 
             // WE DO NOT MEASURE COPY TIME
             // Copy the results back to the host
